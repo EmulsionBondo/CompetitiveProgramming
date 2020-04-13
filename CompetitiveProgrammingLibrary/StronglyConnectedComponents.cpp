10827e00738e7f593a53fecd284ada33a4f675b0
Bondo <tkm.kyudo@gmail.com>
2020-04-13 16:50:31 +0900

    add StronglyConnectedComponents

--- CompetitiveProgrammingLibrary/StronglyConnectedComponents.cpp
+++ CompetitiveProgrammingLibrary/StronglyConnectedComponents.cpp
@@ -1,29 +1,9 @@
-#include <bits/stdc++.h>
-#define REP(i, n) for(int i = 0;i < n;i++)
-#define ll long long
-using namespace std;
-//typedef vector<unsigned int>vec;
-//typedef vector<ll>vec;
-//typedef vector<vec> mat;
-typedef pair<int, int> P;
-typedef pair<ll,ll> LP;
-const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
-const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
-const int INF = 1000000000;
-const ll LINF = 1000000000000000000;//1e18
-const ll  MOD = 1000000007;
-const double PI = acos(-1.0);
-const double EPS = 1e-10;
-
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
-//template<class T> inline void add(T &a, T b){a = ((a+b) % MOD + MOD) % MOD;};
-
 class StronglyConnectedComponents{
     int nodeNum;
     int size;
+public:
     vector<vector<int>> edge,revEdge;
-
+    vector<vector<int>> graph;
     vector<int> label,visited,order;
     // 帰りがけ順に番号を振っていく
     void dfs(int cur){
@@ -38,17 +18,10 @@ class StronglyConnectedComponents{
         for(int next:revEdge[cur]) if(!visited[next]) revDfs(next,labelId);
     }
 
-public:
-
-	StronglyConnectedComponents(const int& nodeNum) : 
+	StronglyConnectedComponents(const int& nodeNum) :
     nodeNum(nodeNum), edge(nodeNum), revEdge(nodeNum), label(nodeNum), visited(nodeNum) {}
 
-    // vが属している強連結成分のグループ（トポロジカル順序）
-	int operator[](int v) {
-		return label[v];
-	}
-
-    void makeEdge(const int from,const int to) {
+    void make_edge(const int from,const int to) {
         edge[from].push_back(to);
         revEdge[to].push_back(from);
     }
@@ -61,19 +34,30 @@ public:
         int labelId = 0;
         for(int i:order) if(!visited[i]) revDfs(i,labelId++);
         size = labelId;
+        make_graph();
+    }
+
+    // 強連結成分で縮約したグラフを作る
+    void make_graph(){
+        graph.resize(size);
+        for(int i = 0; i < edge.size(); i++){
+            for(auto to : edge[i]){
+                if(getLabel(i) == getLabel(to)) continue;
+                graph[getLabel(i)].push_back(getLabel(to));
+            }
+        }
     }
 
     void print(void) {
         for(auto labelId:label) cout << labelId << " ";
         cout << endl;
     }
+    // vが属している強連結成分のラベル（トポロジカル順序）
+    int getLabel(int v){
+        return label[v];
+    }
+
     int getSize(){
         return size;
     }
 };
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    cout << ~(-1) << endl;
-}
