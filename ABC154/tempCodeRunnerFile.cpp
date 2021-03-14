960231ec7ecea15bc5b0e8e19f79396c8880a7e4
Bondo <tkm.kyudo@gmail.com>
2021-03-14 19:17:21 +0900

    整理

--- ABC154/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-REP(i,r+1){
-        res += rec(k+1, tight && i == r, (i == 0 ? zero + 1 : zero));
-    }
--- CF635/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-for(int i=1;i<=nr;i++){
-    //     auto bitr = lower_bound(B.begin(), B.end(), R[i]);
-    //     auto gitr = lower_bound(G.begin(), G.end(), R[i]);
-    //     ll tmp = 0;
-    //     tmp += (R[i] - *bitr) * (R[i] - *bitr);
-    //     tmp += (R[i] - *gitr) * (R[i] - *gitr);
-    //     tmp += (*gitr - *bitr) * (*gitr - *bitr);
-    //     chmin(ans, tmp);
-    // }
