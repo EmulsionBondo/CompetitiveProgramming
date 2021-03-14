d3db8f5d24a27ccf9d9365bc765f99ace9d22406
Bondo <tkm.kyudo@gmail.com>
2021-03-14 19:20:47 +0900

    整理

--- keyence2020/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-if(cnt == 4){
-        REP(i,N){
-            int num, flip, idx;
-            tie(num, flip, idx) = v[i];
-            cout << num << " " << flip << " " << idx << endl;
-        }
-    }
